0e5935602cf2c451b2928e93a8dcd0fae6bcf6f5
tunas6 <jonathancastro0629@gmail.com>
2025-02-07 21:15:36 -0600

    actividad 7 v1

--- Actividad 7 - Interfaces/Interfaces.py
+++ Actividad 7 - Interfaces/Interfaces.py
@@ -1,28 +1,23 @@
 import tkinter as tk
-from ventanas_cursos import VentanasCursos as VC
+from ventanas_cursos import VentanasCursos
 
-def ventana_cursos():
-    ventana_curso = tk.Toplevel(root)
-    ventana_curso.title("Cursos")
-    ventana_curso.geometry("200x300")
-    
-    tk.Button(ventana_curso, text="Insertar", command=VC.ventana_insertar).pack(pady=10)
-    tk.Button(ventana_curso, text="Ver", command=VC.ventana_ver).pack(pady=10)
-    tk.Button(ventana_curso, text="Modificar", command=VC.ventana_modificar).pack(pady=10)
-    tk.Button(ventana_curso, text="Eliminar", command=VC.ventana_eliminar).pack(pady=10)
-    tk.Button(ventana_curso, text="Menú", command=ventana_curso.destroy).pack(pady=10)
+def main():
+    root = tk.Tk()
+    root.title("Menú")
+    root.geometry("200x200")
 
-root = tk.Tk()
-root.title("Menú")
-root.geometry("200x200")
+    vc = VentanasCursos(root)
 
-btn1 = tk.Button(root, text="Curso", command=ventana_cursos)
-btn1.pack(pady=5)
+    btn1 = tk.Button(root, text="Curso", command=vc.ventana_cursos)
+    btn1.pack(pady=5)
 
-btn2 = tk.Button(root, text="Estudiantes")
-btn2.pack(pady=5)
+    btn2 = tk.Button(root, text="Estudiantes")
+    btn2.pack(pady=5)
 
-btn3 = tk.Button(root, text="Inscritos")
-btn3.pack(pady=5)
+    btn3 = tk.Button(root, text="Inscritos")
+    btn3.pack(pady=5)
 
-root.mainloop()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
--- Actividad 7 - Interfaces/ventanas_cursos.py
+++ Actividad 7 - Interfaces/ventanas_cursos.py
@@ -1,147 +1,166 @@
 import tkinter as tk
-from Curso import Curso as c
+from Curso import Curso
 
 class VentanasCursos:
-    @staticmethod
-    def ventana_insertar():
-        ventana = tk.Toplevel()
+    def __init__(self, root):
+        self.root = root
+        self.cursos = Curso()
+        self.cursos.obtener_json()
+
+    def ventana_cursos(self):
+        ventana_curso = tk.Toplevel(self.root)
+        ventana_curso.title("Cursos")
+        ventana_curso.geometry("200x300")
+
+        tk.Button(ventana_curso, text="Insertar", command=self.ventana_insertar).pack(pady=10)
+        tk.Button(ventana_curso, text="Ver", command=self.ventana_ver).pack(pady=10)
+        tk.Button(ventana_curso, text="Modificar", command=self.ventana_modificar).pack(pady=10)
+        tk.Button(ventana_curso, text="Eliminar", command=self.ventana_eliminar).pack(pady=10)
+        tk.Button(ventana_curso, text="Menú", command=ventana_curso.destroy).pack(pady=10)
+
+    def ventana_insertar(self):
+        ventana = tk.Toplevel(self.root)
         ventana.title("Agregar Curso")
         ventana.geometry("500x500")
 
-        label_nombre = tk.Label(ventana, text="Nombre:")
-        label_nombre.pack(pady=5)
-
+        tk.Label(ventana, text="Nombre:").pack(pady=5)
         input_nombre = tk.Entry(ventana)
         input_nombre.pack(pady=5)
 
-        label_descripcion = tk.Label(ventana, text="Descripción:")
-        label_descripcion.pack(pady=5)
-
+        tk.Label(ventana, text="Descripción:").pack(pady=5)
         input_descripcion = tk.Entry(ventana)
         input_descripcion.pack(pady=5)
 
-        label_fecha_inicio = tk.Label(ventana, text="Fecha de inicio:")
-        label_fecha_inicio.pack(pady=5)
-
+        tk.Label(ventana, text="Fecha de inicio:").pack(pady=5)
         input_fecha_inicio = tk.Entry(ventana)
         input_fecha_inicio.pack(pady=5)
 
-        label_fecha_fin = tk.Label(ventana, text="Fecha de fin:")
-        label_fecha_fin.pack(pady=5)
-
+        tk.Label(ventana, text="Fecha de fin:").pack(pady=5)
         input_fecha_fin = tk.Entry(ventana)
         input_fecha_fin.pack(pady=5)
 
-        label_profesor = tk.Label(ventana, text="Profesor:")
-        label_profesor.pack(pady=5)
-
+        tk.Label(ventana, text="Profesor:").pack(pady=5)
         input_profesor = tk.Entry(ventana)
         input_profesor.pack(pady=5)
 
-        btn_guardar = tk.Button(
-            ventana,
-            text="Guardar",
-            command=lambda: print(
+        def agregar_curso():
+            cur = Curso(
                 input_nombre.get(),
                 input_descripcion.get(),
                 input_fecha_inicio.get(),
                 input_fecha_fin.get(),
-                input_profesor.get(),
-            ),
+                input_profesor.get()
+            )
+
+            json = self.cursos.obtener_json()
+
+            self.cursos.json_a_objeto(json)
+
+            cursos = self.cursos.agregar(cur)
+
+            self.cursos.transformar_json("curso")
+
+        btn_agregar = tk.Button(ventana, text="Agregar", command=agregar_curso)
+        btn_agregar.pack(pady=20)
+
+        btn_guardar = tk.Button(
+            ventana,
+            text="Guardar",
+            command=agregar_curso
         )
         btn_guardar.pack(pady=20)
 
-    @staticmethod
-    def ventana_modificar():
-        ventana = tk.Toplevel()
+    def ventana_modificar(self):
+        ventana = tk.Toplevel(self.root)
         ventana.title("Modificar Curso")
         ventana.geometry("500x500")
 
-        label_id = tk.Label(ventana, text="ID:")
-        label_id.pack(pady=5)
-
+        tk.Label(ventana, text="ID:").pack(pady=5)
         input_id = tk.Entry(ventana)
         input_id.pack(pady=5)
 
-        label_nombre = tk.Label(ventana, text="Nombre:")
-        label_nombre.pack(pady=5)
-
+        tk.Label(ventana, text="Nombre:").pack(pady=5)
         input_nombre = tk.Entry(ventana)
         input_nombre.pack(pady=5)
 
-        label_descripcion = tk.Label(ventana, text="Descripción:")
-        label_descripcion.pack(pady=5)
-
+        tk.Label(ventana, text="Descripción:").pack(pady=5)
         input_descripcion = tk.Entry(ventana)
         input_descripcion.pack(pady=5)
 
-        label_fecha_inicio = tk.Label(ventana, text="Fecha de inicio:")
-        label_fecha_inicio.pack(pady=5)
-
+        tk.Label(ventana, text="Fecha de inicio:").pack(pady=5)
         input_fecha_inicio = tk.Entry(ventana)
         input_fecha_inicio.pack(pady=5)
 
-        label_fecha_fin = tk.Label(ventana, text="Fecha de fin:")
-        label_fecha_fin.pack(pady=5)
-
+        tk.Label(ventana, text="Fecha de fin:").pack(pady=5)
         input_fecha_fin = tk.Entry(ventana)
         input_fecha_fin.pack(pady=5)
 
-        label_profesor = tk.Label(ventana, text="Profesor:")
-        label_profesor.pack(pady=5)
-
+        tk.Label(ventana, text="Profesor:").pack(pady=5)
         input_profesor = tk.Entry(ventana)
         input_profesor.pack(pady=5)
 
-        btn_guardar = tk.Button(
-            ventana,
-            text="Guardar",
-            command=lambda: print(
-                input_id.get(),
+        def modificar_curso():
+            index = int(input_id.get())
+            nuevo_curso = Curso(
                 input_nombre.get(),
                 input_descripcion.get(),
                 input_fecha_inicio.get(),
                 input_fecha_fin.get(),
-                input_profesor.get(),
-            ),
-        )
+                input_profesor.get()
+            )
+
+            json = self.cursos.obtener_json()
+            self.cursos.json_a_objeto(json)
+
+            self.cursos.modificar(index, nuevo_curso)
+
+            self.cursos.transformar_json("curso")
+
+        btn_guardar = tk.Button(ventana, text="Guardar", command=modificar_curso)
         btn_guardar.pack(pady=20)
 
-    @staticmethod
-    def ventana_eliminar():
-        ventana = tk.Toplevel()
+    def ventana_eliminar(self):
+        ventana = tk.Toplevel(self.root)
         ventana.title("Eliminar Curso")
         ventana.geometry("500x500")
 
-        label_id = tk.Label(ventana, text="ID:")
-        label_id.pack(pady=5)
-
+        tk.Label(ventana, text="ID:").pack(pady=5)
         input_id = tk.Entry(ventana)
         input_id.pack(pady=5)
+
+        def eliminar_curso():
+            curso = input_id.get()
+            
+            self.cursos.eliminar(int(curso))
 
         btn_eliminar = tk.Button(
             ventana,
             text="Eliminar",
-            command=lambda: print(f"Eliminar curso con ID: {input_id.get()}"),
+            command=eliminar_curso
         )
         btn_eliminar.pack(pady=20)
 
-    @staticmethod
-    def ventana_ver():
+    def ventana_ver(self):
         ventana = tk.Toplevel()
         ventana.title("Ver Cursos")
         ventana.geometry("500x500")
 
-        cursos = tk.Text(ventana)
-        cursos.pack(pady=20, fill=tk.BOTH, expand=True)
+        text_area = tk.Text(ventana)
+        text_area.pack(pady=20, fill=tk.BOTH, expand=True)
 
-        cursos = c.obtener_json()
+        cursos = self.cursos.obtener_json()
 
+        if not cursos:
+            text_area.insert(tk.END, "No hay cursos disponibles.\n")
+            return
+        
+        cursosI = 0
         for curso in cursos:
-            cursos.insert(tk.END, f"ID: {curso['id']}\n")
-            cursos.insert(tk.END, f"Nombre: {curso['nombre']}\n")
-            cursos.insert(tk.END, f"Descripción: {curso['descripcion']}\n")
-            cursos.insert(tk.END, f"Fecha de inicio: {curso['fecha_inicio']}\n")
-            cursos.insert(tk.END, f"Fecha de fin: {curso['fecha_fin']}\n")
-            cursos.insert(tk.END, f"Profesor: {curso['profesor']}\n")
-            cursos.insert(tk.END, "-" * 50 + "\n")
+            text_area.insert(tk.END, f"Id de curso {cursosI}:\n")
+            text_area.insert(tk.END, f"Nombre: {curso.get('nombre', 'Desconocido')}\n")
+            text_area.insert(tk.END, f"Descripción: {curso.get('descripcion', 'No disponible')}\n")
+            text_area.insert(tk.END, f"Fecha de inicio: {curso.get('fecha_inicio', 'No disponible')}\n")
+            text_area.insert(tk.END, f"Fecha de fin: {curso.get('fecha_fin', 'No disponible')}\n")
+            text_area.insert(tk.END, f"Profesor: {curso.get('profesor', 'No disponible')}\n")
+            text_area.insert(tk.END, "----------------------------------------------------------\n")
+            cursosI += 1
